48785aa1acb07cf30638627eb6d618f629cd632e
zenaldo <zenaldo.221@hotmail.com>
2025-01-14 23:16:56 -0400

    configurações iniciais e criação do header

---
+++ src/Components/Header.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+
+import IconUser from "../assets/icon-user.svg";
+import LogoItau from "../assets/logo.svg";
+import ItemMenu from './ItemMenu';
+import * as S from "./styles";
+
+export default function Header() {
+  return (
+    <S.Header>
+      <S.Container>
+        <S.ContainerItens>
+          <div>
+            <S.img src={LogoItau} alt="logo itau" />
+          </div>
+
+          <S.ul>
+
+            <S.li>
+              <ItemMenu name="Para você" />
+            </S.li>
+
+            <S.li>
+              <ItemMenu name="Para empresas" />
+            </S.li>
+
+            <S.li>
+              <ItemMenu name="Serviços" />
+            </S.li>
+
+            <S.li>
+              <ItemMenu name="Ajuda" />
+            </S.li>
+
+          </S.ul>
+          <S.DivButtonAccess>
+            <S.ButtonAccess>
+              <img src={IconUser} alt="icon user" />
+              <span>Acessar conta</span>
+            </S.ButtonAccess>
+          </S.DivButtonAccess>
+        </S.ContainerItens>
+      </S.Container>
+    </S.Header>
+  )
+}
